485f28ec8b60457affd6106389a83fc90672a82d
javier <javierfdb@gmail.com>
2023-03-09 13:42:09 -0300

    cambios pagina notfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,9 +3,13 @@ import {Link} from 'react-router-dom'
 export default function NotFoundt() {
     return (
        <>
-            <div className="container">
-                <h2> Ups! Pokemon no encontrado.</h2>
-                <Link className='btn btn-primary' to="/">Buscar de nuevo</Link>
+            <div className="container my-5">
+                <div className="row">
+                    <div className="col-lg-8">
+                    <h2> Ups! Pokemon no encontrado.</h2>
+                    <Link className='btn btn-primary mt-3' to="/">Buscar de nuevo</Link>
+                    </div>
+                </div>
             </div>
        </>
     )
